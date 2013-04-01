c0951b4e1cf498f187c8a575d91bfee23d63a85e
dottymind <dottymind@gmail.com>
2013-04-01 16:39:14 +0300

    Add error handling.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express'),
     routes = require(path.join(__dirname, 'app', 'routes'));
 
 var app = express();
+app.set('env', process.env.NODE_ENV || 'development');
 app.set('host', process.env.HOST || '127.0.0.1');
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +18,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 if('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
+
+app.use(function(req, res, next) {
+  res.status(404);
+  if(req.accepts('html')) {
+    res.render('404', {url: req.url});
+    return;
+  }
+  if(req.accepts('json')) {
+    res.send({error: 'Not found'});
+    return;
+  }
+  res.type('txt').send('Not found');
+});
+
+/* NOTE: set application's env value different to development to enable this error handler. */
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('500', {error: err});
+});
 
 routes(app);
 
--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,19 @@ module.exports = function(app) {
   app.get('/', function(req, res) {
     res.render('index');
   });
+
+  app.get('/403', function(req, res, next) {
+    var err = new Error('Not allowed');
+    err.status = 403;
+    next(err);
+  });
+
+  app.get('/404', function(req, res, next) {
+    next();
+  });
+
+  app.get('/500', function(req, res, next) {
+    next(new Error('Server error'));
+  });
 }
 
